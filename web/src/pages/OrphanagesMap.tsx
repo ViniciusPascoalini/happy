58c0f2ed49b7d0d45a3197bf0bbc039bcae12ace
Vinicius Pascoalini <vinicius_guilhermep@hotmail.com>
2020-11-03 14:51:05 -0300

    Update OrphanagesMap.tsx

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Map } from 'leaflet'
+import { Map, TileLayer } from 'leaflet';
 
 import { FiPlus } from 'react-icons/fi';
 
@@ -27,8 +27,13 @@ function OrphanagesMap() {
             </aside>
 
             <Map
-                center = "@-20.5375776,-47.404319,15.21z"
-            />
+                center={[-20.5375776,-47.404319]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+            </Map>
                 
 
             <Link to="" className="create-orphanage">
